bd5096c8665a7e6316ad2cc4d5b94d810b907206
Esteban <jomariux4@gmail.com>
2022-03-23 16:07:08 -0600

    Replace buttons with numbers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('.number-button');
 const screen = document.querySelector('.screen');
 let displayValue;
 
